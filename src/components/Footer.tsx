50a4a623a1b2d273f434316dc10d19aedb5d92af
Mathilbzel <buachompoo_r@cmu.ac.th>
2024-09-08 00:49:03 +0700

    Huh

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,10 @@
-import { Title, Rating, Textarea, Button } from '@mantine/core';
+import { Text } from '@mantine/core';
+import { FooterProps } from '@lib/types';
 
-export default function AddReview() {
+export default function Footer({year, name, studentId}: FooterProps){
   return (
     <>
-      <Title order={4} mt="sm" >Your rating</Title>
-
-      <Rating defaultValue={0} size="lg" />
-
-      <Textarea
-        mt="sm"
-        label="Your review"
-        placeholder="Do you enjoy eating?"
-        rows={3}
-      />
-
-      <Button variant="filled" color="orange" size="sm" mt="sm" >Submit Review</Button>
+      <Text ta="center" c="dimmed" my="sm" fz={15.2} >Copyright © {year} {name} {studentId}</Text>
     </>
   );
 }
